6ba8bbe9a87716da3104d65b34d388be03d47406
WhyWeStillHere <runb2000@gmail.com>
2021-10-21 21:57:26 +0300

    Small fix

--- lab1/branch_diff.sh
+++ lab1/branch_diff.sh
@@ -4,7 +4,7 @@ b1=$2
 b2=$3
 workdir="__tmp_dir__"
 if [ -f "$workdir" ]; then
-    rm $workdir
+    rm -r $workdir
 fi
 mkdir $workdir
 cd $workdir
@@ -14,4 +14,4 @@ git checkout -q $b2
 diff=$(git diff --name-status "$b1..$b2")
 printf "$diff\n" 
 cd ..
-rm -rf $workdir
+rm -r $workdir
