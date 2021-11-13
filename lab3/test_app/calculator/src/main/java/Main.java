fca2f6c21eb29a75a581c8a7f4cbb7aaa7c84b98
whywestillhere <runb2000@gmail.com>
2021-11-13 18:39:55 +0300

    Initial test app

---
+++ lab3/test_app/calculator/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    double a = 2;
+    double b = 2;
+
+    System.err.printf("Result of %.2f * %.2f: %.2f", a, b, AdvancedCalculator.mult(a, b));
+  }
+}
---
+++ lab3/test_app/calculator/src/test/java/AdvancedCalculatorTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AdvancedCalculatorTest {
+  @Test
+  public void multTest_zero() {
+    assertEquals(0, AdvancedCalculator.mult(5, 0));
+    assertEquals(0, AdvancedCalculator.mult(0, 5));
+    assertEquals(0, AdvancedCalculator.mult(0, 0));
+  }
+
+  @Test
+  public void multTest_simpleTest() {
+    assertEquals(2.5, AdvancedCalculator.mult(5, 0.5));
+    assertEquals(4, AdvancedCalculator.mult(2, 2));
+    assertEquals(-10, AdvancedCalculator.mult(2, -5));
+    assertEquals(-10, AdvancedCalculator.mult(-2.5, 4));
+    assertEquals(100, AdvancedCalculator.mult(-10, -10));
+  }
+}
